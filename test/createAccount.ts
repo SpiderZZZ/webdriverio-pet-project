e999bb58d93e644ede781288b709a21a8576f462
Ryagin Yurij <y.ryagin@xrm.ru>
2020-11-16 19:33:51 +0500

    Created first test in webdriverIO

---
+++ test/createAccount.ts
@@ -0,0 +1,24 @@
+describe('User',() => //describe - аналог сьюта 
+{
+    it('can register', () =>
+    {
+        browser.url('/create_account');     
+        const registrationForm = $('#box-create-account');
+        registrationForm.$('input[name="company"]').setValue('Название компании');
+        registrationForm.$('input[name="firstname"]').setValue('Иван');
+        registrationForm.$('input[name="lastname"]').setValue('Иванов');
+        const countrySelect = registrationForm.$('select[name="country_code"]');
+        countrySelect.selectByVisibleText("Russian Federation");
+        const randomEmail = `test${new Date().getTime()/1000}@mail.ru`
+        registrationForm.$('input[name="email"]').setValue(randomEmail);
+        registrationForm.$('input[name="password"]').setValue('testtest');
+        registrationForm.$('input[name="confirmed_password"]').setValue('testtest');
+        registrationForm.$('button[name="create_account"]').click();
+    
+
+        browser.pause(5000)
+        const expectedText = 'Your customer account has been created.'
+        const alert = $('#notices .alert-success')
+        expect(alert).toHaveTextContaining(expectedText)
+    })
+})
